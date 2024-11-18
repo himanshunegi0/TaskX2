78f6faf5437bd8a1519d32cbdf9d83df1d8cb54e
himanshunegi0 <116428077+himanshunegi0@users.noreply.github.com>
2024-11-19 00:16:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+
+
+
+
+
+//https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Update this if your site is deployed under a subpath
+  plugins: [react()],
 })
-
-
-
-
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
